bccebd73a0536ffe01bd380746b6ba140b2dddd6
aryangulati <42711978+aryangulati@users.noreply.github.com>
2020-06-05 21:59:39 +0530

    .

---
+++ PYTHON_BASICS/customname.py
@@ -0,0 +1,15 @@
+def hello_to_all(name):
+    """
+    this function return greeting
+    """
+    print(f"{name} says hello to all")
+
+    
+def bye(name):
+    print(f"{name} say bye to all")
+
+def add(a,b):
+    return(a+b)
+
+def print_name():
+    print(__name__)
